5579e5b8fe4917b20f4774574f3b1ec21871ff46
UlrikKryhl <uchris23@student.aau.dk>
2026-05-08 08:32:38 +0200

    updated todos

--- src/main/java/p4project/ParserDriver.java
+++ src/main/java/p4project/ParserDriver.java
@@ -12,10 +12,9 @@ import p4project.visitors.RefLinkingVisitor;
 import p4project.visitors.TypeCheckingVisitor;
 import p4project.visitors.FtableGenVisitor;
 
-// TODO - add arrayLiteral
-// TODO - add arrayIndex
-// TODO - add critical section
-// TODO - add shared mutexes
+// TODO - unit testing for each visitor phase
+// TODO - integration testing for the whole pipeline
+// TODO - acceptance testing with sample programs and expected outputs
 
 public class ParserDriver {
     public static void main(String[] args) {
