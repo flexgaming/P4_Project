6f46e545a5d83e04a761920b354276f999db1586
flexgaming <skinkelynet06@gmail.com>
2026-05-16 22:27:15 +0200

    Update ArrayValidator.java
    Last clean up

--- src/main/java/p4project/context/ArrayValidator.java
+++ src/main/java/p4project/context/ArrayValidator.java
@@ -14,22 +14,19 @@ public class ArrayValidator {
         int firstComma = input.indexOf(",");
         int dimensions = (int) input.substring(0, firstComma).chars().filter(ch -> ch == '{').count();
 
-        int[] arrLiteral = new int[dimensions]; // Used to uphold each dimension's rightfull size, based on the first input.
+        int[] arrLiteral = new int[dimensions]; // Used to uphold each dimension's rightful size, based on the first input.
         int[] dimensionSize = new int[dimensions]; // Used to store the current dimension's size, and is used to compare with arrLiteral.
 
         Arrays.fill(arrLiteral, -1); // None of the dimension sizes has been set yet.
-        Arrays.fill(dimensionSize, 1); // Setting 1 foreach index, instead of adding it after the commas has been counted. 
-        int depth = 0;
+        Arrays.fill(dimensionSize, 1); // Setting 1 foreach index, instead of adding it after the commas has been counted.
         int currentDepth = 0;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == '{') {
-                depth++; 
                 currentDepth++;
             } else if (c == ',') dimensionSize[currentDepth - 1]++;
             else if (c == '}') {
-                depth--;
                 
                 if (arrLiteral[currentDepth - 1] == -1) { // Has the dimension size been set? If not set the size.
                     arrLiteral[currentDepth - 1] = dimensionSize[currentDepth - 1];
@@ -41,7 +38,7 @@ public class ArrayValidator {
             }
             
         }
-        if (depth != 0) {
+        if (currentDepth != 0) {
             throw new RuntimeException("You have an uneven amount of curly brackets in context: " + input);
         }
 
@@ -55,22 +52,19 @@ public class ArrayValidator {
      * @return Is an int[] with the size of the dimensions and contains each dimension's correct size.
      */
     public int[] validate(String input, ArrayTypeSymbol base) {
-        int[] arrLiteral = new int[base.dimensions]; // Used to uphold each dimension's rightfull size, based on the first input.
+        int[] arrLiteral = new int[base.dimensions]; // Used to uphold each dimension's rightful size, based on the first input.
         int[] dimensionSize = new int[base.dimensions]; // Used to store the current dimension's size, and is used to compare with arrLiteral
 
         Arrays.fill(arrLiteral, -1); // None of the dimension sizes has been set yet.
         Arrays.fill(dimensionSize, 1); // Setting 1 foreach index, instead of adding it after the commas has been counted.
-        int depth = 0;
         int currentDepth = 0;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == '{') {
-                depth++; 
                 currentDepth++;
             } else if (c == ',') dimensionSize[currentDepth - 1]++;
             else if (c == '}') {
-                depth--;
                 
                 String expectedStr = base.dimSize[currentDepth - 1]; // Expected size of the dimension 'currentDepth - 1'.
                 if (expectedStr != null && expectedStr.matches("\\d+")) { // Check if string only consist of digits 0-9.
@@ -89,7 +83,7 @@ public class ArrayValidator {
                 currentDepth--;
             }
         }
-        if (depth != 0) {
+        if (currentDepth != 0) {
             throw new RuntimeException("You have an uneven amount of curly brackets in context: " + input);
         }
 
